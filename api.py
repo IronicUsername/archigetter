2293e460108638eb2adb785a69d8698826482b6e
IronicUsername <deniztoma512@gmail.com>
2020-03-28 16:35:34 +0100

    add id to payload + CORs

--- api.py
+++ api.py
@@ -1,9 +1,16 @@
+import json
+
 from bs4 import BeautifulSoup
 import cssutils
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+origins = ['*']
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 checklist = [['div', 'screenbg', 'style'], ['div', 'screenfg', 'style'], ['img', 'buffer', 'src']]
 
 
@@ -23,6 +30,9 @@ def getTV():
             elem = soup.find(ids[0], {'id': ids[1]})[ids[2]]
             elem_src = (cssutils.parseStyle(elem)['background-image'][4:-1] if ids[2] != 'src' else elem)
             res[ids[1]] = elem_src
+
+        res['gifid'] = soup.find('div', {'id': 'gifid'}).text[1:]
+        res['gifid_buffer'] = soup.find('img', {'id': 'buffer'})['index']
     else:
-        res = {'message': 'something went wrong.'}
+        res = {'message': 'something went wrong.', 'error': r.text}
     return res
